37abe690b63ed05bd5a214f0a8766929ae335182
Khamilah Nixon <knixon4@ggc.edu>
2022-07-26 12:34:02 -0400

    adds files to repo

---
+++ src/Greeting.java
@@ -0,0 +1,28 @@
+import java.util.ArrayList;
+import java.util.HashMap;
+
+public class Greeting {
+    private final String GREETING;
+
+    public Greeting(String greeting) {
+        GREETING = greeting;
+    }
+
+    public String getGREETING() {
+        return GREETING;
+    }
+
+    public HashMap<Character,Integer> countChar() {
+        HashMap<Character,Integer> map = new HashMap<>();
+        for(int i = 0; i < GREETING.length(); i++) {
+            char letter = GREETING.charAt(i);
+            if(!map.containsKey(letter)) {
+                map.put(letter,1);
+            }
+            else {
+                map.replace(letter,map.get(letter) + 1);
+            }
+        }
+        return map;
+    }
+}
---
+++ src/Price.java
@@ -0,0 +1,10 @@
+public class Price {
+    private Greeting greeting;
+    private GreetingCard card;
+
+    private final double
+            COST_PER_UPPER = .05,
+            COST_PER_LOWER = .02,
+            COST_PER_SPECIAL = .03,
+            COST_PER_NUMBER = .02;
+}
