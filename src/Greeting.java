ae8c7f482874dc7e0731355a7cce0b06e7a06d0a
Khamilah Nixon <knixon4@ggc.edu>
2022-07-26 15:26:45 -0400

    adds total method

--- src/Greeting.java
+++ src/Greeting.java
@@ -6,9 +6,37 @@ public class Greeting {
     private final String GREETING;
     private final HashMap<Character,Integer> charCount;
 
+    private final double
+            COST_PER_UPPER = .05,
+            COST_PER_LOWER = .02,
+            COST_PER_SPECIAL = .03,
+            COST_PER_NUMBER = .02;
+
     public Greeting(String greeting) {
         GREETING = greeting;
         charCount = countChar();
+    }
+    private double total() {
+        double total = 0;
+        Iterator<Map.Entry<Character, Integer>> iterator = iterator();
+        while(iterator.hasNext()) {
+            Map.Entry<Character,Integer> next = iterator.next();
+            char letter = next.getKey();
+            int count = next.getValue();
+            if(Character.isUpperCase(letter)) {
+                total += COST_PER_UPPER * count;
+            }
+            else if(Character.isLowerCase(letter)) {
+                total += COST_PER_LOWER * count;
+            }
+            else if(Character.isDigit(letter)) {
+                total += COST_PER_NUMBER * count;
+            }
+            else {
+                total += COST_PER_SPECIAL * count;
+            }
+        }
+        return total;
     }
 
     public String getGREETING() {
@@ -35,10 +63,4 @@ public class Greeting {
     public Iterator<Map.Entry<Character, Integer>> iterator() {
         return charCount.entrySet().iterator();
     }
-
-    @Override
-    public String toString() {
-        return "Greeting: " + GREETING;
-    }
-
 }
